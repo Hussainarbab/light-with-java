b6661962dcbb6320d2e1ab5e99d2893a9d5315e5
Hussainarbab <arbabhussan63@gmail.com>
2025-07-17 14:21:47 -0700

    commit change

---
+++ app.js
@@ -0,0 +1,30 @@
+
+
+
+let a=prompt("enter number for check day");
+let day;
+
+if(a = 1){
+    day= "monday";
+}else if(a = 2){
+
+    day ="tusesday"
+}else if(a = 3){
+
+    day ="wednesday"
+}else if(a = 4){
+
+    day ="thrusday"
+}else if(a = 5){
+
+    day ="friday"
+}else if(a = 5){
+
+    day ="saturday"
+}else if(a = 7){
+
+    day ="sunday"
+}else{
+    alert("not valid input")
+}
+alert(day)
