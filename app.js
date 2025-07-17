792f5e156142f282f95e33f080ef819da6a62930
Hussainarbab <arbabhussan63@gmail.com>
2025-07-17 14:28:44 -0700

    change commit

--- app.js
+++ app.js
@@ -1,30 +1,57 @@
 
 
 
-let a=prompt("enter number for check day");
-let day;
+// let a=prompt("enter number for check day");
+// let day;
 
-if(a = 1){
-    day= "monday";
-}else if(a = 2){
+// if(a = 1){
+//     day= "monday";
+// }else if(a = 2){
 
-    day ="tusesday"
-}else if(a = 3){
+//     day ="tusesday"
+// }else if(a = 3){
 
-    day ="wednesday"
-}else if(a = 4){
+//     day ="wednesday"
+// }else if(a = 4){
 
-    day ="thrusday"
-}else if(a = 5){
+//     day ="thrusday"
+// }else if(a = 5){
 
-    day ="friday"
-}else if(a = 5){
+//     day ="friday"
+// }else if(a = 5){
 
-    day ="saturday"
-}else if(a = 7){
+//     day ="saturday"
+// }else if(a = 7){
 
-    day ="sunday"
-}else{
-    alert("not valid input")
+//     day ="sunday"
+// }else{
+//     alert("not valid input")
+// }
+// alert(day)
+let a = Number(prompt("Enter a number")); // Convert input to number
+
+switch (a) {
+  case 1:
+    alert("Monday");
+    break;
+  case 2:
+    alert("Tuesday");
+    break;
+  case 3:
+    alert("Wednesday");
+    break;
+  case 4:
+    alert("Thursday");
+    break;
+  case 5:
+    alert("Friday");
+    break;
+  case 6:
+    alert("Saturday");
+    break;
+  case 7:
+    alert("Sunday");
+    break;
+  default:
+    alert("Not a valid input");
 }
-alert(day)
